c5cd1ee08608e2f3ea9993ea63bfbbba7419d440
vincedavincii <99784095+vincedavincii@users.noreply.github.com>
2022-10-21 13:26:58 +0300

    added footer and hero icons

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer className="absolute bottom-0 left-[50%] translate-x-[-50%] whitespace-nowrap p-6 text-sm text-gray-500">
+			<p>Copyright &copy; {new Date().getFullYear()} Vincent</p>
+		</footer>
+	);
+};
+
+export default Footer;
